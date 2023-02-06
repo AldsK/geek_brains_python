70abdd6acbde04776d70a7dc895600b781e2d74e
Aldmsk <Aldsmsk@gmail.com>
2023-02-06 18:42:11 +0300

    выполнил второе задание

--- task.py
+++ task.py
@@ -1,6 +1,13 @@
-x = int(input('Введите число:'))
-x1 = x + x
-x2 = x + x + x
-x3 = x + x + x + x
-x4 = x + x + x + x + x
-print(x, x1, x2, x3, x4, sep='---',)
+# x = int(input('Введите число:'))
+# x1 = x + x
+# x2 = x + x + x
+# x3 = x + x + x + x
+# x4 = x + x + x + x + x
+# print(x, x1, x2, x3, x4, sep='---',)
+
+# задание два
+name = 'Kostya'
+print(len(name))
+i = 0
+for i in name:
+    print(i)
