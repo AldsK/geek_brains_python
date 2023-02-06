90224213c39d323945a59326488c01e262a28001
Aldmsk <Aldsmsk@gmail.com>
2023-02-06 17:52:37 +0300

    внес правки  и добавил задачу

--- task.py
+++ task.py
@@ -1,4 +1,8 @@
 print('hello world')
 # Задание номер 1
 print('1', '2', '3', sep='*')
-print('1', '2', '3', '4', sep= '^', end='finish')
+print('1', '2', '3', '4', sep='^', end='\n')
+# Задание номер 3
+num = int(input('Введите число:\n'))
+result = num - 1
+print(result)
